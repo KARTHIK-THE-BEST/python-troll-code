75e78c8747de0c0d83400b59176a0479a8842e74
KARTHIK-THE-BEST <76962891+KARTHIK-THE-BEST@users.noreply.github.com>
2021-01-04 22:21:06 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,41 @@
+#written by karthik
+#TROLL PROGRAM
+#feel free to send this to someone you hate
+import time
+import webbrowser
+print("THIS PROGRAM ACCURATELY TELLS ABOUT YOU")
+x=int(input("press[(1)if you are a boy]  [(2)if you are a girl]  :"))
+y=int(input("press[(1)if you are popular] [(2)if you are lonely]  :"))
+z=int(input("press[(1)if you have more money :) ]  [(2)if you are broke :( ]  :"))
+time.sleep(1)
+print("analysing your chracter")
+time.sleep(2)
+if (x==1):
+    if(y==1):
+        if(z==1):
+            print("looks like u are smart or is it? ")
+        else:
+            print("likely u are a noob HAHA")
+    else:
+        if(z==1):
+            print("you are stingy BOOMER")
+        else:
+            print("you are not worth anything LOL")
+if (x==2):
+    if(y==1):
+        if(z==1):
+            print("you really are blessed to SUCK ")
+        else:
+            print("you need a boyfriend,so that you make him broke too :)")
+    else:
+        if(z==1):
+            print("sell your properties and do some good things JERK")
+        else:
+            print("you must stop spending money on cosmetics LMAO")
+time.sleep(2)
+print("MORE ANALYSIS ON RESPONSES FOR ACCURATE RESULTS ..........")
+time.sleep(1)
+print("final results will open a video about you in youtube:")
+time.sleep(2)
+webbrowser.open('https://www.youtube.com/watch?v=eXRWKel1zK0')                       
+                                                         
