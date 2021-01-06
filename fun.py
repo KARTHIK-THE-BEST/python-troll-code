3deb45e75f1d848c514b035296e026031a88d877
KARTHIK-THE-BEST <76962891+KARTHIK-THE-BEST@users.noreply.github.com>
2021-01-06 12:51:26 +0530

    Update fun.py
    send this to someone u hate the most. easily customisible for more spicyness. written in python in literally 2 minutes. feel free to use (no copyrights).  🥇

--- fun.py
+++ fun.py
@@ -3,7 +3,7 @@
 #feel free to send this to someone you hate
 import time
 import webbrowser
-print("THIS PROGRAM ACCURATELY TELLS ABOUT YOU")
+print("THIS PROGRAM ACCURATELY TELLS ABOUT YOU")   #try changing the options which suit you
 x=int(input("press[(1)if you are a boy]  [(2)if you are a girl]  :"))
 y=int(input("press[(1)if you are popular] [(2)if you are lonely]  :"))
 z=int(input("press[(1)if you have more money :) ]  [(2)if you are broke :( ]  :"))
@@ -14,28 +14,69 @@ if (x==1):
     if(y==1):
         if(z==1):
             print("looks like u are smart or is it? ")
+            k=1
         else:
             print("likely u are a noob HAHA")
+            k=2
     else:
         if(z==1):
             print("you are stingy BOOMER")
+            k=3
         else:
             print("you are not worth anything LOL")
+            k=4
 if (x==2):
     if(y==1):
         if(z==1):
             print("you really are blessed to SUCK ")
+            k=5
         else:
             print("you need a boyfriend,so that you make him broke too :)")
+            k=6
     else:
         if(z==1):
             print("sell your properties and do some good things JERK")
+            k=7
         else:
             print("you must stop spending money on cosmetics LMAO")
+            k=8
+else:
+   print("enter a valid response dumbass")
 time.sleep(2)
 print("MORE ANALYSIS ON RESPONSES FOR ACCURATE RESULTS ..........")
 time.sleep(1)
 print("final results will open a video about you in youtube:")
 time.sleep(2)
-webbrowser.open('https://www.youtube.com/watch?v=eXRWKel1zK0')                       
-                                                         
+if(k==1):
+  print("this is the meaning of you")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=eXRWKel1zK0')  #u suck meme
+if(k==2):
+  print("damn sure you are him in disguise")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')  #rickrooled
+if(k==3):
+  print("you will end up like .....")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=j9V78UbdzWI')  #astronomia
+if(k==4):
+  print("this is your status ")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=4wX2xBOuzRg')  #FBI open up
+if(k==5):
+  print("you are one of those dancers")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=LDU_Txk06tM')  #crab rave
+if(k==6):
+  print("wow you have super power of ......")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=QH2-TGUlwu4')  #nyan cat
+if(k==7):
+  print("you are noisy like that ......")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=i1H0leZhXcY')  #barking dog
+if(k==8):
+  print("you are too cool like those .......")
+  time.sleep(2)
+  webbrowser.open('https://www.youtube.com/watch?v=sFYo2-KUeTA')  #thomas meme
+  #you change the youtube video link to which you want                                                       
